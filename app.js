7482a6c87198170e0c211de47d277e17a8c327d0
Zachary Powers <zachapowers@gmail.com>
2019-08-08 10:08:21 -0600

    schemas defined

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
 const layout = require('./views/layout');
+const { db } = require('./models');
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 app.get("/", (req, res) => {
   console.log('get on /');
---
+++ models/index.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+
+const Page = db.define ( 'page', {
+   title: {
+        type: Sequelize.STRING
+   },
+   slug: {
+       type: Sequelize.STRING
+   },
+   content: {
+       type: Sequelize.TEXT
+   },
+   status: {
+       type: Sequelize.ENUM('open', 'closed'),
+   }
+})
+
+const User = db.define( 'user',{
+   name: {
+       type: Sequelize.STRING
+   },
+   email: {
+       type: Sequelize.STRING
+   }
+})
+
+
+module.exports = {
+  db, Page, User
+}
